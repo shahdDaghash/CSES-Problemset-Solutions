1b667de593a77f80786eee9e4a34064d05a846c5
shahdDaghash <78550854+shahdDaghash@users.noreply.github.com>
2022-02-07 17:28:42 +0200

    Create Bit Strings.cpp

---
+++ Introductory Problems/Bit Strings.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define FAST ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define scanArray(a,n) for(int i = 0; i < n; i++){cin >> a[i];}
+#define coutArray(a,n) for(int i = 0; i < n; i++){cout << a[i] << " ";};cout << endl;
+#define MOD 1000000007
+#define PI 3.141592653589793
+#define ll long long
+ 
+using namespace std;
+ 
+ll fastpow(int v,  ll p){
+    if (p == 0)return 1;
+    if (p == 1)return v;
+    long long ans = fastpow(v, p/2)%MOD;
+    ans *= ans;
+    ans %= MOD;
+    if (p%2 == 1)return (ans * v)%MOD;
+    else return ans;
+}
+ 
+int main()
+{
+    FAST
+    //int t; cin>>t;
+    int t = 1;
+    while(t--)
+    {
+        int n; cin>>n;
+        cout<<fastpow(2,n)%MOD;
+ 
+    }
+    return 0;
+}
